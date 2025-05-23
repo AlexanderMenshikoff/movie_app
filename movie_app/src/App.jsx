e752e783e6deb9869ce22ac9a2cd3eac3476f618
Alexander <alexmenshikov7@gmail.com>
2025-05-23 13:05:30 +0500

    added new components on main page

--- movie_app/src/App.jsx
+++ movie_app/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import Paragraph from './components/Paragraph/Paragraph';
+import Input from './components/Input/Input';
+import Button from './components/Button/Button';
 
 function App() {
 
 	return (
 		<>
-			<div></div>
+			<Navbar/>
+			<Header text="Поиск"/>
+			<Paragraph text="Введите название фильма, сериала или мультфильма для поиска и добавления в избранное."/>
+			<Input img="/search.svg" placeholder="Введите название"/>
+			<Button text="Искать"/>
 		</>
 	);
 
