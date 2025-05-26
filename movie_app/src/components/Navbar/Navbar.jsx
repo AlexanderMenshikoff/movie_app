7dcdeebfdd81a74987cc69865172a6d69f689608
Alexander <alexmenshikov7@gmail.com>
2025-05-26 23:18:47 +0500

    renamed svg path

--- movie_app/src/components/Navbar/Navbar.jsx
+++ movie_app/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 const Navbar = () => {
 	return(
 		<div className='navbar'>
-			<img src="/logo.svg" alt="img"/>
+			<img src="svg/logo.svg" alt="img"/>
 			<ul className='link-list'>
 				<li className='link-item'>Поиск фильмов</li>
 				<li className='link-item'>Мои фильмы</li>
